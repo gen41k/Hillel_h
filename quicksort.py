59c6faf6f825bb6e29e0a86c76f7c0a3eb27f736
gen41k <statusgroupko2@gmail.com>
2023-04-09 16:29:44 +0100

    m

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,6 @@
 import time
 
+
 def partition(data, head, tail, drawData, timeTick):
     border = head
     pivot = data[tail]
@@ -18,44 +19,44 @@ def partition(data, head, tail, drawData, timeTick):
         drawData(data, getColorArray(len(data), head, tail, border, j))
         time.sleep(timeTick)
 
-
-    #swap pivot with border value
+    # swap pivot with border value
     drawData(data, getColorArray(len(data), head, tail, border, tail, True))
     time.sleep(timeTick)
 
     data[border], data[tail] = data[tail], data[border]
-    
+
     return border
+
 
 def quick_sort(data, head, tail, drawData, timeTick):
     if head < tail:
         partitionIdx = partition(data, head, tail, drawData, timeTick)
 
-        #LEFT PARTITION
-        quick_sort(data, head, partitionIdx-1, drawData, timeTick)
+        # LEFT PARTITION
+        quick_sort(data, head, partitionIdx - 1, drawData, timeTick)
 
-        #RIGHT PARTITION
-        quick_sort(data, partitionIdx+1, tail, drawData, timeTick)
+        # RIGHT PARTITION
+        quick_sort(data, partitionIdx + 1, tail, drawData, timeTick)
 
 
-def getColorArray(dataLen, head, tail, border, currIdx, isSwaping = False):
+def getColorArray(dataLen, head, tail, border, currIdx, isSwaping=False):
     colorArray = []
     for i in range(dataLen):
-        #base coloring
+        # base coloring
         if i >= head and i <= tail:
-            colorArray.append('gray')
+            colorArray.append("gray")
         else:
-            colorArray.append('white')
+            colorArray.append("white")
 
         if i == tail:
-            colorArray[i] = 'blue'
+            colorArray[i] = "blue"
         elif i == border:
-            colorArray[i] = 'red'
+            colorArray[i] = "red"
         elif i == currIdx:
-            colorArray[i] = 'yellow'
+            colorArray[i] = "yellow"
 
         if isSwaping:
             if i == border or i == currIdx:
-                colorArray[i] = 'green'
+                colorArray[i] = "green"
 
     return colorArray
